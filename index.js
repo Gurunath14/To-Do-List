35f55bcdcdebea300c5a488ac0f722747a8d2ad5
Gurunath14 <103202155+Gurunath14@users.noreply.github.com>
2023-11-13 22:44:33 +0530

    created an todo list project

---
+++ index.js
@@ -0,0 +1,37 @@
+var a=document.querySelector(".popbg");
+var b=document.querySelector(".popup");
+var c=document.querySelector(".circle");
+var d=document.getElementById("cancel")
+
+c.addEventListener("click",function(){
+    a.style.display="block";
+    b.style.display="block";
+})
+d.addEventListener("click",function(){
+    a.style.display="none";
+    b.style.display="none";
+})
+
+//getting add button and the input values of the popup
+var h=document.querySelector(".book-title");
+var e=document.getElementById("add");
+var i=document.querySelector(".container");
+var j=document.querySelector(".book-author");
+var k=document.querySelector(".short-des");
+e.addEventListener("click",function(){
+    var f=document.createElement("div");
+    f.setAttribute("class","bookcontainer");
+    f.innerHTML=`<h2>${h.value}</h2>
+    <h4>${j.value}</h4>
+    <p>${k.value}</p>
+    <button onclick="deletebutton(event)">Delete</button>`
+    i.append(f);
+    a.style.display="none";
+    b.style.display="none";
+})
+
+
+function deletebutton(event){
+    event.target.parentElement.remove();
+
+}
