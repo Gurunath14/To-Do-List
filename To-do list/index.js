caa4fd02eed94c44ff6194e0d8678671b70ef65a
Gurunath14 <gurunath014@gmail.com>
2024-01-25 23:40:46 +0530

    made changes in js file

--- To-do list/index.js
+++ To-do list/index.js
@@ -2,21 +2,24 @@ var a = document.querySelector(".popbg");
 var b = document.querySelector(".popup");
 var c = document.querySelector(".circle");
 var d = document.getElementById("cancel");
+
 c.addEventListener("click", function () {
   a.style.display = "block";
   b.style.display = "block";
 });
+
 d.addEventListener("click", function () {
   a.style.display = "none";
   b.style.display = "none";
 });
 
-//getting add button and the input values of the popup
+// getting add button and the input values of the popup
 var h = document.querySelector(".book-title");
 var e = document.getElementById("add");
 var i = document.querySelector(".container");
 var j = document.querySelector(".book-author");
 var k = document.querySelector(".short-des");
+
 e.addEventListener("click", function () {
   var f = document.createElement("div");
   f.setAttribute("class", "bookcontainer");
@@ -25,15 +28,16 @@ e.addEventListener("click", function () {
     <p>${k.value}</p>
     <button onclick="deletebutton(event)">Delete</button>`;
   i.append(f);
-  savedata();
+
+  // Clear input values
+  h.value = "";
+  j.value = "";
+  k.value = "";
+
   a.style.display = "none";
   b.style.display = "none";
 });
 
 function deletebutton(event) {
   event.target.parentElement.remove();
-}
-
-function savedata() {
-  localStorage.setItem("data", i.innerHTML);
 }
